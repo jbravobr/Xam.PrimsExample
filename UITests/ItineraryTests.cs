964ace9d276b9d5fd44c15b8ddd9d9ebb69546ca
Rodrigo Amaro <ramaral@icatuseguros.com.br>
2016-05-03 15:38:29 -0300

    Configuração Testes e Interfaces Itinerary

---
+++ UITests/ItineraryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace IcatuzinhoApp.UITests
+{
+    [TestFixture]
+    public class ItineraryTests
+    {
+        HttpClient _httpClient = Helpers.ReturnClient();
+
+        [Test]
+        public async Task Try_Get_itineraries()
+        {
+            var listItineraries = new List<Itinerary>();
+
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var result = await _httpClient.GetAsync($"icatuzinhoapi/api/itinerary/");
+
+            if (result.IsSuccessStatusCode)
+            {
+                var stringJson = await result.Content.ReadAsStringAsync();
+                listItineraries = JsonConvert.DeserializeObject<List<Itinerary>>(stringJson);
+            }
+
+            CollectionAssert.AllItemsAreInstancesOfType(listItineraries, typeof(Station));
+            CollectionAssert.IsNotEmpty(listItineraries);
+            CollectionAssert.AllItemsAreNotNull(listItineraries);
+        }
+    }
+}
+
